0ddf88bde026103d10158b9da8d788d474e98d25
Sophie Creutz <sophie.creutz@flatironschool.com>
2019-09-27 13:14:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,43 @@ function preventRefreshOnSubmit(){
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation(){
+  var input = document.querySelector('input[name="name"]')
+  return input.value.toString();
+}
+
+function addNewElementAsLi(){
+var li = document.createElement('li')
+  var string = retrieveEmployeeInformation()
+  li.innerHTML = string
+  var element = document.getElementsByClassName('employee-list')[0]
+  element.appendChild(li)
+  
+} 
+
+ 
+
+function addNewLiOnClick(){
+ 
+  addNewElementAsLi();
+  document.querySelector('input[name="name"]').value='';
+  
+}
+
+var submit = document.querySelector('input[name=""]')
+  submit.addEventListener("click", () => addNewLiOnClick);
+  
+  var clear = document.querySelector("a[href='#']")
+  
+  
+function clearEmployeeListOnLinkClick(){
+  var list = document.getElementsByClassName('employee-list')
+    var e = document.querySelector("ul"); 
+  e.length = 0; 
+  e.innerHTML = '';
+  
+}
+
+clear.addEventListener("click", () => clearEmployeeListOnLinkClick);
+
